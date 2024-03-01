622e262d50b820acca41ca572b9022f94543a01f
Omar_Ameer <omar.ameer244@gmail.com>
2024-03-01 21:31:37 +0200

    New Camera Position with a New Target and Zoom.

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CustomGoogleMap extends StatefulWidget {
@@ -21,24 +22,44 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   }
 
   @override
+  void dispose() {
+    googleMapController.dispose();
+    super.dispose();
+  }
+
+  late GoogleMapController googleMapController;
+
+  @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         GoogleMap(
+          onMapCreated: (controller) {
+            googleMapController = controller;
+          },
           initialCameraPosition: initialCameraPosition,
-          cameraTargetBounds: CameraTargetBounds(
-            LatLngBounds(
-              northeast: const LatLng(30.828787393718397, 31.01620939037795),
-              southwest: const LatLng(30.76265444706483, 30.971685935161435),
-            ),
-          ),
+          // cameraTargetBounds: CameraTargetBounds(
+          //   LatLngBounds(
+          //     northeast: const LatLng(30.828787393718397, 31.01620939037795),
+          //     southwest: const LatLng(30.76265444706483, 30.971685935161435),
+          //   ),
+          // ),
         ),
         Positioned(
           bottom: 16,
           left: 16,
           right: 16,
           child: ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              // The class is named "CameraUpdate" as it update the whole camera attributes like position, zoom, ... . not just the Latlng().
+              CameraPosition newLocation = const CameraPosition(
+                target: LatLng(24.714896340717083, 46.68578759605556),
+                zoom: 13,
+              );
+              googleMapController.animateCamera(
+                CameraUpdate.newCameraPosition(newLocation),
+              );
+            },
             child: const Text("Change Location"),
           ),
         ),
